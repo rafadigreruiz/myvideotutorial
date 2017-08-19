7cc8d6ca90fcfcdf03aeee25b08ac399fcffe3b4
AQUILES ROJAS <AQUILES ROJAS@AQUILESROJAS-PC>
2017-08-18 20:15:59 -0400

    Initial commit

---
+++ zk-test-01/src/main/java/org/test/zk/dao/CPerson.java
@@ -0,0 +1,57 @@
+package org.test.zk.dao;
+
+import java.io.Serializable;
+
+public class CPerson implements Serializable {
+
+    private static final long serialVersionUID = -154217276901168581L;
+    
+    protected String strId;
+    protected String strFirstName;
+    protected String strLastName;
+    
+    //Constructor
+    public CPerson( String strId, String strFirstName, String strLastName ) {
+        
+        this.strId = strId;
+        this.strFirstName = strFirstName;
+        this.strLastName = strLastName;
+        
+    }
+    
+    public String getId() {
+        
+        return strId;
+    }
+
+    
+    public void setId( String strId ) {
+        
+        this.strId = strId;
+    }
+
+    
+    public String getFirstName() {
+        
+        return strFirstName;
+    }
+
+    
+    public void setFirstName( String strFirstName ) {
+        
+        this.strFirstName = strFirstName;
+    }
+
+    
+    public String getLastName() {
+        
+        return strLastName;
+    }
+
+    
+    public void setLastName( String strLastName ) {
+        
+        this.strLastName = strLastName;
+    }
+    
+}
